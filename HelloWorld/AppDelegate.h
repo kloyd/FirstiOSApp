294594a323b5495cb6d814ac480754ca482f0ee8
Kelly Loyd <kelly@argent-5.local>
2018-05-21 21:17:18 -0500

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Kelly Loyd on 5/21/18.
+//  Copyright © 2018 WaldoAndMagic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
